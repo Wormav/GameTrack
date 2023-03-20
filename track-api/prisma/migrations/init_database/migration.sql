36af16d417ad8fcc43fa47436264c0161fa2cfcd
Jérémy Lorette <jeremy.lorette@outlook.com>
2023-03-20 21:33:02 +0100

    .env db_url

---
+++ track-api/prisma/migrations/init_database/migration.sql
@@ -0,0 +1,64 @@
+-- CreateTable
+CREATE TABLE "Games" (
+    "id" SERIAL NOT NULL,
+    "game_id" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "release_date" TIMESTAMP(3) NOT NULL,
+    "publisher" TEXT NOT NULL,
+    "platform_id" INTEGER NOT NULL,
+    "update_at" TIMESTAMP(3) NOT NULL,
+    "genre_id" INTEGER NOT NULL,
+    "multiplayer" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Games_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Platform" (
+    "id" SERIAL NOT NULL,
+    "genre_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "logo" TEXT NOT NULL,
+    "games_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Platform_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Genre" (
+    "id" SERIAL NOT NULL,
+    "genre_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "logo" TEXT NOT NULL,
+    "games_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Genre_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "bio" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "is_active" BOOLEAN NOT NULL,
+    "picture" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Platform" ADD CONSTRAINT "Platform_games_id_fkey" FOREIGN KEY ("games_id") REFERENCES "Games"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Genre" ADD CONSTRAINT "Genre_games_id_fkey" FOREIGN KEY ("games_id") REFERENCES "Games"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
