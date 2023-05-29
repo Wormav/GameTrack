d907fcf03e58ada46bfff46be0991717762d5d4f
Jérémy Lorette <jeremy.lorette@outlook.com>
2023-05-30 00:41:38 +0200

    add verifyJwt middleware in all games routes

--- track-api/src/routes/games.routes.ts
+++ track-api/src/routes/games.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 import { addGameInUserGames, deleteGameInUserGames,
 getAllUserGames, getGames, getOneGame } from '../controllers/games.controllers';
+import { verifyJwt } from '../middlewares/verifyJwt';
 
 const gamesRouter = Router();
 
-gamesRouter.get('/games', getGames)
-gamesRouter.get('/game/:id', getOneGame)
-gamesRouter.get('/usergames', getAllUserGames)
-gamesRouter.post('/addgame', addGameInUserGames)
-gamesRouter.delete('/deletegame', deleteGameInUserGames)
+gamesRouter.get('/games',verifyJwt, getGames)
+gamesRouter.get('/game/:id',verifyJwt, getOneGame)
+gamesRouter.get('/usergames',verifyJwt, getAllUserGames)
+gamesRouter.post('/addgame',verifyJwt, addGameInUserGames)
+gamesRouter.delete('/deletegame',verifyJwt, deleteGameInUserGames)
 
 export default gamesRouter;
