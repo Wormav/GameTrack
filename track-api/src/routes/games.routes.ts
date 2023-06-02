07a855df5f293d1183ff137dc97a411d9a8b835d
Jérémy Lorette <jeremy.lorette@outlook.com>
2023-06-02 21:31:04 +0200

    add verifyJwt for route getGames

--- track-api/src/routes/games.routes.ts
+++ track-api/src/routes/games.routes.ts
@@ -1,14 +1,19 @@
-import { Router } from 'express'
-import { addGameInUserGames, deleteGameInUserGames,
-getAllUserGames, getGames, getOneGame } from '../controllers/games.controllers';
-import { verifyJwt } from '../middlewares/verifyJwt';
+import { Router } from "express";
+import {
+  addGameInUserGames,
+  deleteGameInUserGames,
+  getAllUserGames,
+  getGames,
+  getOneGame,
+} from "../controllers/games.controllers";
+import { verifyJwt } from "../middlewares/verifyJwt";
 
 const gamesRouter = Router();
 
-gamesRouter.get('/games', getGames)
-gamesRouter.get('/game/:id',verifyJwt, getOneGame)
-gamesRouter.get('/usergames',verifyJwt, getAllUserGames)
-gamesRouter.post('/addgame',verifyJwt, addGameInUserGames)
-gamesRouter.delete('/deletegame',verifyJwt, deleteGameInUserGames)
+gamesRouter.get("/games", verifyJwt, getGames);
+gamesRouter.get("/game/:id", verifyJwt, getOneGame);
+gamesRouter.get("/usergames", verifyJwt, getAllUserGames);
+gamesRouter.post("/addgame", verifyJwt, addGameInUserGames);
+gamesRouter.delete("/deletegame", verifyJwt, deleteGameInUserGames);
 
 export default gamesRouter;
