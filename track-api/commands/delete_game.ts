b89b79e242299e4d6c577692ab1e4a994fe3f877
dimitri hauet <dimitrihauet@gmail.com>
2023-07-15 20:43:54 +0200

    create delete game command

---
+++ track-api/commands/delete_game.ts
@@ -0,0 +1,37 @@
+import { Option, program } from 'commander';
+import * as dotenv from 'dotenv'
+import { IResponseGame, IgdbClient } from '../src/queries/igdb_client';
+import cliProgress from 'cli-progress';
+import colors from 'ansi-colors';
+import { deleteGame } from '../src/database/clients/games.client';
+
+dotenv.config()
+
+program
+  .addOption(new Option('-i, --id <number>', 'id of the game').argParser(parseInt))
+  .addOption(new Option('-n, --name <string>', 'name of the game').argParser(String))
+
+program.parse(process.argv);
+
+const options = program.opts();
+const id = options.id as number;
+const name = options.name as string;
+
+if (!id && !name) {
+  console.error("You must provide either an id or a name");
+  process.exit(1);
+}
+
+function main() {
+  deleteGame(id, name)
+    .then((title: string) => {
+      console.log(`Game ${title} deleted`);
+    }
+    )
+    .catch((err) => {
+      console.error(err);
+    } )
+  
+}
+
+main();
