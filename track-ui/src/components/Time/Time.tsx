580983dd5e007a91ee1b8eb4426443495cc7f34a
Jérémy Lorette <jeremy.lorette@outlook.com>
2023-08-08 00:21:24 +0200

    feat: add cont time and state for open modal

--- track-ui/src/components/Time/Time.tsx
+++ track-ui/src/components/Time/Time.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { UserGamesContext } from '@src/contexts/UserGamesContext';
 import { StyledTime, StyledEditIcon } from './time.styles';
 
@@ -8,18 +8,21 @@ interface TimeProps {
 }
 
 export default function Time({ gameId, gameInUserGames }: TimeProps) {
+  const [openModal, setOpenModal] = useState(false);
+
   const { userGames } = useContext(UserGamesContext);
 
   const game = userGames?.find((g) => g.game_id === gameId);
-  // pour utiliser game_time dans le span
+  const time = game?.game_time?.main_story;
 
   return (
     <StyledTime>
       {gameInUserGames ? (
         <>
           <h1>Temps de jeu :</h1>
-          <StyledEditIcon />
-          <span>2 jours 5 heures 23 minutes</span>
+          <StyledEditIcon onClick={() => setOpenModal(true)} />
+          {openModal && <h1>Modal</h1>}
+          <span>{time || 'Aucun temps renseigné'}</span>
         </>
       ) : (
         <h1>Ajouter ce jeu pour ajouter un temps de jeu</h1>
